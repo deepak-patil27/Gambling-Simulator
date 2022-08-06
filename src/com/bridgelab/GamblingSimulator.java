6af5b2add9b7d769c623b5f99ebfe26d81aca3b2
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-07 01:35:08 +0530

    Added: Welcome To Gambling Simulator

---
+++ src/com/bridgelab/GamblingSimulator.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class GamblingSimulator {
+	public static void main(String[] args) {
+		System.out.println("---------Welcome To Gambling Simulator---------");
+	}
+
+}
