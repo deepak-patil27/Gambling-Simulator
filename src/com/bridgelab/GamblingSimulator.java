bd978af99f690d4a014239c4e219ea8c1a789586
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-07 02:05:40 +0530

    Added: Gambler Either Win Or Loose

--- src/com/bridgelab/GamblingSimulator.java
+++ src/com/bridgelab/GamblingSimulator.java
@@ -1,14 +1,30 @@
 package com.bridgelab;
 
 public class GamblingSimulator {
-	static int INITIAL_STAKE = 100;
-    static int STAKE_BET = 1;
-	
+	static double INITIAL_STAKE = 100;
+	static double STAKE_BET = 1;
+	public static int stake = 0;
+
+	public static void winOrLose() {
+
+		if (Math.random() < 0.5) {
+			INITIAL_STAKE++;
+			System.out.println("\nPlayer Win");
+			;
+		} else {
+			INITIAL_STAKE--;
+			System.out.println("\nPlayer Lose");
+
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("---------Welcome To Gambling Simulator---------");
 		System.out.println();
-		System.out.println("Amount of stake is -> " + INITIAL_STAKE );
-		 System.out.println("Amount to bet after every game -> " + STAKE_BET);
-	}
+		System.out.println("Amount of stake is -> " + INITIAL_STAKE);
+		System.out.println("Amount to bet after every game -> " + STAKE_BET);
+		winOrLose();
+		System.out.println("\nAvailable Amount of Stake -> " + INITIAL_STAKE );
 
+	}
 }
