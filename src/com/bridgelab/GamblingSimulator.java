dfecdeb8e7708d68d05faf0250eb592e3111edce
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-07 01:44:46 +0530

    Added: Condition For Stake And Bet

--- src/com/bridgelab/GamblingSimulator.java
+++ src/com/bridgelab/GamblingSimulator.java
@@ -1,8 +1,14 @@
 package com.bridgelab;
 
 public class GamblingSimulator {
+	static int INITIAL_STAKE = 100;
+    static int STAKE_BET = 1;
+	
 	public static void main(String[] args) {
 		System.out.println("---------Welcome To Gambling Simulator---------");
+		System.out.println();
+		System.out.println("Amount of stake is -> " + INITIAL_STAKE );
+		 System.out.println("Amount to bet after every game -> " + STAKE_BET);
 	}
 
 }
